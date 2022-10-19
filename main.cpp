5e11572476d6d80c6e6e1a680e926ded2e4030db
people pineapple <113289837+Nilapine@users.noreply.github.com>
2022-10-20 00:17:15 +0700

    Bilangan lebih dan kurang dari

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 	int x, y;
+	bool kondisi;
+	string hasil;
+
+	// operasi >
+	kondisi = x < y ;
+
+
+	cout << "Masukkan Bilangan x : ";
+	cin >> x;
+	cout << "Masukkan Bilangan y : ";
+	cin >> y;
+
+	if ( x > y == 1 ) {cout << "variabel x > dari variabel y \n";}
+	else {cout << "variabel x < dari variabel y";}
+
+}
